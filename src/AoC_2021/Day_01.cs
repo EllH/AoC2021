98ac63dec7c63cc6d453e00c11310b503d1d72f5
elliot <ellhiggs96@gmail.com>
2021-12-03 00:48:38 +0000

    Clean up day2 solution and removed redundant namespace

--- src/AoC_2021/Day_01.cs
+++ src/AoC_2021/Day_01.cs
@@ -5,30 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using AoCHelper;
 
-namespace AoC_2021
+public class Day01 : BaseDay
 {
-    public class Day01 : BaseDay
+    private readonly string input;
+
+    private readonly List<int> inputAsInts;
+
+    public Day01()
     {
-        private readonly string input;
+        input = File.ReadAllText(InputFilePath);
+        inputAsInts = Array.ConvertAll(input.Split("\n"), int.Parse).ToList();
+    }
 
-        private readonly List<int> inputAsInts;
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>(inputAsInts.Skip(1).Zip(inputAsInts, (curr, prev) => curr > prev)
+            .Count(c => c).ToString());
+    }
 
-        public Day01()
-        {
-            input = File.ReadAllText(InputFilePath);
-            inputAsInts = Array.ConvertAll(input.Split("\n"), int.Parse).ToList();
-        }
-
-        public override ValueTask<string> Solve_1()
-        {
-            return new ValueTask<string>(inputAsInts.Skip(1).Zip(inputAsInts, (curr, prev) => curr > prev)
-                .Count(c => c).ToString());
-        }
-
-        public override ValueTask<string> Solve_2()
-        {
-            return new ValueTask<string>(inputAsInts.Skip(3).Zip(inputAsInts, (curr, prev) => curr > prev)
-                .Count(c => c).ToString());
-        }
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>(inputAsInts.Skip(3).Zip(inputAsInts, (curr, prev) => curr > prev)
+            .Count(c => c).ToString());
     }
 }
--- src/AoC_2021/Day_02.cs
+++ src/AoC_2021/Day_02.cs
@@ -1,69 +1,69 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AoCHelper;
-
-namespace AoC_2021
+public class Day02 : BaseDay
 {
-    public class Day02 : BaseDay
+    private readonly string input;
+
+    public Day02()
     {
-        private readonly string input;
-
-        public Day02()
-        {
-            input = File.ReadAllText(InputFilePath);
-        }
-    
-        public override ValueTask<string> Solve_1()
-        {
-            string[] inputSplit = input.Split("\n").ToArray();
-            var depth = 0;
-            var horizontal = 0;
-            foreach (var row in inputSplit)
-            {
-                var line = row.Split(" ");
-
-                switch (line.First())
-                {
-                    case "forward":
-                        depth += int.Parse(line.Last());
-                        break;
-                    case "up":
-                        horizontal -= int.Parse(line.Last());
-                        break;
-                    case "down":
-                        horizontal += int.Parse(line.Last());
-                        break;
-                }
-            }
-            return new ValueTask<string>((horizontal * depth).ToString());
-        }
-    
-        public override ValueTask<string> Solve_2()
-        {
-            string[] inputSplit = input.Split("\n").ToArray();
-            var depth = 0;
-            var horizontal = 0;
-            var aim = 0;
-            foreach (var row in inputSplit)
-            {
-                var line = row.Split(" ");
-
-                switch (line.First())
-                {
-                    case "forward":
-                        horizontal += int.Parse(line.Last());
-                        depth += int.Parse(line.Last()) * aim;
-                        break;
-                    case "up":
-                        aim -= int.Parse(line.Last());
-                        break;
-                    case "down":
-                        aim += int.Parse(line.Last());
-                        break;
-                }
-            }
-            return new ValueTask<string>((horizontal * depth).ToString());
-        }
+        input = File.ReadAllText(InputFilePath);
     }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>(Parse(input).Aggregate(new State(0, 0, 0), (state, parsedInput) =>
+            parsedInput.direction switch
+            {
+                "forward" => state with
+                {
+                    depth = state.depth + parsedInput.amount,
+                },
+                "up" => state with
+                {
+                    horizontal = state.horizontal - parsedInput.amount
+                },
+                "down" => state with
+                {
+                    horizontal = state.horizontal + parsedInput.amount
+                },
+                _ => throw new Exception("Direction Not Found.")
+            }, state => state.horizontal * state.depth).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>(Parse(input).Aggregate(new State(0, 0, 0), (state, parsedInput) =>
+            parsedInput.direction switch
+            {
+                "forward" => state with
+                {
+                    horizontal = state.horizontal + parsedInput.amount,
+                    depth = state.depth + parsedInput.amount * state.aim,
+                },
+                "up" => state with
+                {
+                    aim = state.aim - parsedInput.amount
+                },
+                "down" => state with
+                {
+                    aim = state.aim + parsedInput.amount
+                },
+                _ => throw new Exception("Direction Not Found.")
+            }, state => state.horizontal * state.depth).ToString());
+    }
+
+    IEnumerable<Input> Parse(string input) =>
+        from
+            line in input.Split('\n')
+        let parts = line.Split()
+        select
+            new Input(parts[0], int.Parse(parts[1]));
 }
+
+record Input(string direction, int amount);
+
+record State(int depth, int horizontal, int aim);
