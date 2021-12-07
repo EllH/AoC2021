8bde81866f3c7c1a796f3dbd96f672af682bfba1
elliot <ellhiggs96@gmail.com>
2021-12-07 18:55:48 +0000

    Day 6 re-write and day 7

--- src/AoC_2021/Day_06.cs
+++ src/AoC_2021/Day_06.cs
@@ -9,52 +9,29 @@ public class Day06 : BaseDay
         this.Input = File.ReadAllText(InputFilePath);
     }
 
-    private long[] ParseInput()
+    private long FishCountAfterNDays(string input, int days)
     {
-        var hack = this.Input.Split(",").Select(s => Convert.ToInt64(s)).ToList();
-
-        long[] output = new long[9];
-        
-        output[0] = hack.Count(v => v == 0);
-        output[1] = hack.Count(v => v == 1);
-        output[2] = hack.Count(v => v == 2);
-        output[3] = hack.Count(v => v == 3);
-        output[4] = hack.Count(v => v == 4);
-        output[5] = hack.Count(v => v == 5);
-        output[6] = hack.Count(v => v == 6);
-        output[7] = hack.Count(v => v == 7);
-        output[8] = hack.Count(v => v == 8);
-
-        return output;
-    }
-
-    private static string solver(long[] fishTank, int growthDays)
-    {
-        var oldFishTank = new long[9];
-        for (int i = 0; i < growthDays; i++)
+        var fishCountByInternalTimer = new long[9];
+        foreach (var ch in input.Split(','))
         {
-            fishTank.CopyTo(oldFishTank, 0);
-            fishTank[0] = oldFishTank[1];
-            fishTank[1] = oldFishTank[2];
-            fishTank[2] = oldFishTank[3];
-            fishTank[3] = oldFishTank[4];
-            fishTank[4] = oldFishTank[5];
-            fishTank[5] = oldFishTank[6];
-            fishTank[6] = oldFishTank[7] + oldFishTank[0];
-            fishTank[7] = oldFishTank[8];
-            fishTank[8] = oldFishTank[0];
+            fishCountByInternalTimer[int.Parse(ch)]++;
         }
 
-        return fishTank.Sum().ToString();
+        for (var t = 0; t < days; t++)
+        {
+            fishCountByInternalTimer[(t + 7) % 9] += fishCountByInternalTimer[t % 9];
+        }
+
+        return fishCountByInternalTimer.Sum();
     }
 
     public override ValueTask<string> Solve_1()
     {
-        return new(solver(this.ParseInput(), 80));
+        return new(FishCountAfterNDays(this.Input, 80).ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new(solver(this.ParseInput(), 256));
+        return new(FishCountAfterNDays(this.Input, 256).ToString());
     }
 }
---
+++ src/AoC_2021/Day_07.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+public class Day07 : BaseDay
+{
+    private readonly string Input;
+
+    public Day07()
+    {
+        this.Input = File.ReadAllText(InputFilePath);
+    }
+
+    private List<int> parseInput()
+    {
+        return this.Input.Split(",").Select(int.Parse).ToList();
+    }
+
+    private IEnumerable<IEnumerable<int>> getFuelBaseUsage(IEnumerable<int> crabs)
+    {
+        return Enumerable.Range(0, crabs.Max()).Select(position => crabs.Select(crab2 => Math.Abs(crab2 - position))).ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(getFuelBaseUsage(parseInput()).Min(i => i.Sum()).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(getFuelBaseUsage(parseInput()).Min(i => i.Sum(c => c * (c + 1) / 2)).ToString());
+    }
+}
